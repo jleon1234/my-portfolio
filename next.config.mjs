55f7d93fc5d525f33e7dbabf256f997a4116b27f
Juan Pablo <jleon4811@gmail.com>
2025-01-12 23:43:31 -0500

    add path iamges

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
+
+
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.NODE_ENV === 'production';
+const repoName = 'my-portfolio'; // Replace with your GitHub repository name
+
 const nextConfig = {
+    basePath: isGithubPages ? `/${repoName}` : '',
+    assetPrefix: isGithubPages ? `/${repoName}/` : '',
     output: "export",
     distDir: "dist",
     iamges: {
